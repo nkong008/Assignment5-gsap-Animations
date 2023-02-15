15e80d0cdb51d3f9a7d78dea6d2c35ae2421e5b5
nkong008 <nkong2020@gmail.com>
2023-02-14 20:10:17 -0600

    added design 3

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ $(document).ready(function() {
     //variables
     let paths = $('.flower');
     let rect = $('.building');
+    let path = $('.bird');
+    let ellipse = $('.circle');
+    
     
 
 
@@ -21,6 +24,12 @@ $(document).ready(function() {
     tl2.from(rect , {duration: 0.5, autoAlpha: 0, stagger: 0.3, ease: "back.out"}, );
     tl2.fromTo(rect, {stroke: "#FCFC0D", drawSVG: "0%", duration: 6},{duration: 6, drawSVG: "100%", ease: "back.out(1)", stroke:"#36F8FF"}, 1)
     //end of design 2////
+
+    //start of design 3
+    let tl3 = gsap.timeline();
+    tl3.fromTo(path , {stroke: "#FCFC0D", drawSVG: "0%", duration: 5},{duration: 5, drawSVG: "100%", stroke:"#36F8FF"}, )
+    tl3.fromTo(ellipse , {stroke: "#36F8FF", drawSVG: "0%", duration: 5},{duration: 5, drawSVG: "100%", stroke:"#FCFC0D",stagger: 0.5}, 2 )
+    //end of design 3
 
     GSDevTools.create();
     //jQuery ends here////////////////////
